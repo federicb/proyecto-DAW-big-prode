08957070d707101e9797e972328d6a92f364978a
Federico <isma-fb@hotmail.com>
2023-05-08 19:02:58 +0200

    inicalización proyecto, server ok, ruta y vista index ok

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+//initializations
+const app = express();
+
+//setting
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//routes
+app.use(require('./routes/index.js'));
+
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//server
+app.listen(3000, () => {
+    console.log("Server on port 3000");
+});
