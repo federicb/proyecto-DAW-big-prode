0f57b5b8df6957a902863d2f88e4033e8ded0b3d
Federico <isma-fb@hotmail.com>
2023-05-10 20:15:09 +0200

    contact view created

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,24 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 });
 
+// router.get('/contact', (req, res) => {
+//     res.render('contact');
+// });
+
+// router.post("/contact_user", (req,res) => {
+//     console.log(req.body);
+
+//     //desestructuramos el objeto x comodidad
+//     let {phone,name,email,message} = req.body;
+
+//     //envio de correo
+//     conexion(name,email,phone,message);
+
+
+//     res.redirect("/");
+// });
 
 module.exports = router;
