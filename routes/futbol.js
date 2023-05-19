3aa43d454433d778ae28da2577818c539b695d1b
Federico <isma-fb@hotmail.com>
2023-05-19 21:15:06 +0200

    obtención de datos por fecha y renderizados a forecasts.ejs

--- routes/futbol.js
+++ routes/futbol.js
@@ -11,14 +11,12 @@ router.get('/posiciones', async (req, res) =>{
             headers: {
                 "x-rapidapi-host": "v3.football.api-sports.io",
                 "x-rapidapi-key": "189c6369de01ac78a7e883e97676d7d1"
-                // "x-rapidapi-key": "a6ecc313566f6e7c8c0a3abb265cc7df"
             }
         });
         const data = await response.json();
-        // console.log(data.response);
         // console.log(data.response[0]);
-        console.log(data.response[0].league.standings[1]);
         const standings = data.response[0].league.standings[1];
+        // res.json(data.response[0].league)
         res.render('positions', { standings });
     } catch (err) {
         console.log(err);
@@ -26,57 +24,30 @@ router.get('/posiciones', async (req, res) =>{
     }
 });
 
-// rounds?league=128&season=2023
-// fixtures?live=all
-// fixtures?league=128&season=2023
 // fixtures?league=128&season=2023&round=1st Phase - 9
-router.get('/pronosticos', async (req, res) => {
+router.get('/pronosticos', async (req, res) =>{
+    
     try {
         const response = await fetch("https://v3.football.api-sports.io/fixtures?league=128&season=2023", {
             method: "GET",
             headers: {
                 "x-rapidapi-host": "v3.football.api-sports.io",
                 "x-rapidapi-key": "189c6369de01ac78a7e883e97676d7d1"
-                // "x-rapidapi-key": "a6ecc313566f6e7c8c0a3abb265cc7df"
             }
         });
-        // const data = await response.json();
-        // data.response.forEach((match) => {
-        //     const homeTeamName = match.teams.home.name;
-        //     const awayTeamName = match.teams.away.name;
-        //     const homeGoals = match.goals.home;
-        //     const awayGoals = match.goals.away;
-        //     console.log(homeTeamName, " - ", homeGoals);
-        //     console.log(awayTeamName, " - ", awayGoals);
-        // });
-        // // res.json(data.response);
-        // res.render('forecasts', { matches: data.response });
         const data = await response.json();
-        // console.log(data.response);
-        // Filtrar los partidos por la ronda deseada
-        const round = "1st Phase - 16";
-        const filteredMatches = data.response.filter(match => match.league.round.includes(round));
-        // console.log(filteredMatches);
-        // Extraer la información necesaria de los partidos filtrados
-        const matches = filteredMatches.map(match => {
-            const homeTeamName = match.teams.home.name;
-            const awayTeamName = match.teams.away.name;
-            const homeGoals = match.goals.home;
-            const awayGoals = match.goals.away;
-            console.log(homeTeamName, " - ", homeGoals);
-            console.log(awayTeamName, " - ", awayGoals);
-          return {
-            homeTeamName,
-            awayTeamName,
-            homeGoals,
-            awayGoals
-          };
-        });
-    
-        res.render('forecasts', { matches });
+
+        const filteredMatches = data.response.filter(match => match.league.round.includes('1st Phase'));
+        // Obtén la fecha actual
+        const currentDate = new Date().toISOString().split('T')[0];
+        console.log(currentDate)
+
+        res.render('forecasts', { currentDate, matches: filteredMatches });
+
+
     } catch (err) {
         console.log(err);
-        res.status(500).send("Error al obtener los pronósticos");
+        res.status(500).send("Error al obtener las pronósticos");
     }
 });
 
