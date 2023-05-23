6bd61f278cfd0732fda2b0dbf462ef3e703b5407
Federico <isma-fb@hotmail.com>
2023-05-23 23:49:07 +0200

    profile view, security routes, logout

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,22 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require('../config/auth')
 
-router.get('/register', (req,res) => {
+router.get('/register', isNotLoggedIn, (req,res) => {
     res.render('auth/register')
 });
 
-router.post('/register', passport.authenticate('local.signup', {
+router.post('/register', isNotLoggedIn, passport.authenticate('local.signup', {
     successRedirect: '/profile',
     failureRedirect: '/register',
     failureFlash: true
 }));
 
-router.get('/login', (req,res) => {
+router.get('/login', isNotLoggedIn, (req,res) => {
     res.render('auth/login')
 });
 
-router.post('/login', (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local.signin', {
         successRedirect: '/profile',
         failureRedirect: '/login',
@@ -26,8 +27,15 @@ router.post('/login', (req, res, next) => {
 });
 
 
-router.get('/profile', (req,res) => {
-    res.send('perfil')
+router.get('/profile',isLoggedIn, (req,res) => {
+    res.render('profile')
+});
+
+router.get('/logout', isLoggedIn, (req,res) => {
+    req.logOut(function(err){
+        if (err) return next(err)
+    });
+    res.redirect('/');
 });
 
 module.exports = router;
